ae453a876041bfd460681f297df4581573e392f3
DEVIL-X <72757203+TEAMOFDEVIL-X@users.noreply.github.com>
2021-07-03 16:53:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# TIANA PRO-BOT
+▪️ THIS IS PRO-GROUP MANAGER BOT.
+
+![Deploy](https://telegra.ph/file/bcd7b3dec21d1c2b2583a.png)
+
+## INFORMATION
+
+<p align="center">
+    <a href="https://github.com/TEAMOFDEVIL-X/TIANABOT/stargazers"><img src="https://img.shields.io/github/stars/TEAMOFDEVIL-X/TIANABOT?style=for-the-badge" alt="GitHub stars" /></a>
+    <a href="https://app.codacy.com/manual/TEAMOFDEVIL-X/TIANABOT/dashboard"> <img src="https://img.shields.io/codacy/grade/4d58f2a402b54aed8a7d95f7add45a81?color=brightgreen&logo=codacy&logoColor=green&style=for-the-badge" alt="Codacy" /></a>
+    <a href="https://github.com/TEAMOFDEVIL-X/TIANABOT"> <img src="https://img.shields.io/github/repo-size/TEAMOFDEVIL-X/TIANABOT?color=orange&logo=github&logoColor=green&style=for-the-badge" /></a>
+    <a href="https://github.com/TEAMOFDEVIL-X/TIANABOT/commits/prince"> <img src="https://img.shields.io/github/last-commit/TEAMOFDEVIL-X/TIANABOT?color=brown&logo=github&logoColor=green&style=for-the-badge" /></a>
+    <a href="https://github.com/TEAMOFDEVIL-X/TIANABOT/issues"> <img src="https://img.shields.io/github/issues/TEAMOFDEVIL-X/TIANABOT?color=blueviolet&logo=github&logoColor=green&style=for-the-badge" /></a>
+    <a href="https://github.com/TEAMOFDEVIL-X/TIANABOT/network/members"> <img src="https://img.shields.io/github/forks/TEAMOFDEVIL-X/TIANABOT?color=red&logo=github&logoColor=green&style=for-the-badge" /></a>  
+    <a href="https://pypi.org/project/Telethon/"> <img src="https://img.shields.io/pypi/v/telethon?color=yellow&label=telethon&logo=python&logoColor=green&style=for-the-badge" /></a>
+</p>
+
+## DEPLOY ON HEROKU 
+
+The easiest way to deploy this Bot Is Heroku
+
+<p align="center"><a href="https://heroku.com/deploy?template=https://github.com/TEAMOFDEVIL-X/TIANABOT"> <img src="https://img.shields.io/badge/Deploy%20To%20Heroku-black?style=for-the-badge&logo=heroku" width="220" height="38.45"/></a></p>
+
+BOT MAKER - [PRINCE](https://t.me/DEVILSDAD_PRINCE)
+
+### TELEGRAM
+Check me on [TELEGRAM](https://t.me/tiana_prince_bot)
+
+## THANKS TOO.. 
+=>> ▪️ [MASHAROBOT](https://github.com/Mr-Dark-Prince/MashaRoBot)
+
+=>> ▪️ [TEAMDAISYX](https://github.com/TeamDaisyX)
+
+IF I FORGET TO GIVE YOU CREDITS CONTACT ME ON [TELEGRAM](https://t.me/DEVILSDAD_PRINCE) 
+
---
+++ Tianabot/modules/tianalogo.py
@@ -0,0 +1,86 @@
+from Tianabot.events import register
+from Tianabot import OWNER_ID, BOT_NAME
+from Tianabot import telethn as tbot
+import os 
+from PIL import Image, ImageDraw, ImageFont
+
+@register(pattern="^/logo ?(.*)")
+async def lego(event):
+ quew = event.pattern_match.group(1)
+ if event.sender_id == OWNER_ID:
+     pass
+ else:
+ 
+  if not quew:
+     await event.reply('Darling Listen! Provide Some Text To Draw!')
+     return
+ await event.reply('Creating your logo...wait!')
+ try:
+    text = event.pattern_match.group(1)
+    img = Image.open('./Tianabot/resources/logo/logo.jpg')
+    draw = ImageDraw.Draw(img)
+    image_widthz, image_heightz = img.size
+    pointsize = 500
+    fillcolor = "white"
+    shadowcolor = "blue"
+    font = ImageFont.truetype("./Tianabot/resources/font/Maghrib.ttf", 1000)
+    w, h = draw.textsize(text, font=font)
+    h += int(h*0.21)
+    image_width, image_height = img.size
+    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
+    x = (image_widthz-w)/2
+    y= ((image_heightz-h)/2+6)
+    draw.text((x, y), text, font=font, fill="white", stroke_width=0, stroke_fill="white")
+    fname2 = "LogobyTiana.png"
+    img.save(fname2, "png")
+    await tbot.send_file(event.chat_id, fname2, caption="MADE BY @TIANA_PRINCE_BOT")
+    if os.path.exists(fname2):
+            os.remove(fname2)
+ except Exception as e:
+   await event.reply(f'Error Report @PrinceBotSupport, {e}')
+
+
+@register(pattern="^/wlogo ?(.*)")
+async def lego(event):
+ quew = event.pattern_match.group(1)
+ if event.sender_id == OWNER_ID:
+     pass
+ else:
+ 
+  if not quew:
+     await event.reply('Darling Listen! Provide Some Text To Draw!')
+     return
+ await event.reply('Creating your logo...wait!')
+ try:
+    text = event.pattern_match.group(1)
+    img = Image.open('./Tianabot/resources/logo/logo1.jpg')
+    draw = ImageDraw.Draw(img)
+    image_widthz, image_heightz = img.size
+    pointsize = 500
+    fillcolor = "gold"
+    shadowcolor = "blue"
+    font = ImageFont.truetype("./Tianabot/resources/font/Chopsic.otf", 330)
+    w, h = draw.textsize(text, font=font)
+    h += int(h*0.21)
+    image_width, image_height = img.size
+    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
+    x = (image_widthz-w)/2
+    y= ((image_heightz-h)/2+6)
+    draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="white")
+    fname2 = "LogobyTiana.png"
+    img.save(fname2, "png")
+    await tbot.send_file(event.chat_id, fname2, caption="MADE BY @TIANA_PRINCE_BOT")
+    if os.path.exists(fname2):
+            os.remove(fname2)
+ except Exception as e:
+   await event.reply(f'Error Report @PrinceBotSupport, {e}')
+
+file_help = os.path.basename(__file__)
+file_help = file_help.replace(".py", "")
+file_helpo = file_help.replace("_", " ")
+
+__help__ = """
+ • `/logo text`*:*  Create your logo with your name
+ • `/wlogo`*:*  Create your logo with your name
+ """
+__mod_name__ = "LOGO"
